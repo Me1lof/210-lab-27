e310a3d94b0a60ee6be6def5d0b68d33c20bba8f
me1lof <melissaochoaf04@gmail.com>
2024-11-02 10:37:50 -0700

    commit #3

--- lab27/lab27/lab27.cpp
+++ lab27/lab27/lab27.cpp
@@ -12,4 +12,39 @@ map<string, tuple<int, string, string>> villagers;
 
 void displayVillagers() {
 	cout << "\nVillagers and their details:" << endl;
+	for (auto& pair : villagers) {
+		cout << pair.first << " - Friendship Level: " << get<0>(pair.second)
+			<< ", Species: " << get<1>(pair.second)
+			<< ", Catchphrase: " << get<2>(pair.second) << endl;
+	}
+}
+
+
+int main() {
+	int choice;
+
+	do {
+		cout << "\nMenu:\n"
+			<< "1. Add Villager\n"
+			<< "2. Display All Villagers\n"
+			<< "3. Delete Villager\n"
+			<< "4. Search for Villager\n"
+			<< "5. Increase Friendship Level\n"
+			<< "6. Decrease Friendship Level\n"
+			<< "7. Exit\n"
+			<< "Enter your choice: ";
+		cin >> choice;
+
+
+		switch (choice) {
+			case 1: 
+			case 2: displayVillagers(); break;
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+			case 7:
+		}
+	}
+	return 0;
 }
